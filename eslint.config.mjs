93f22bd6887c4db00f39ccfcb0836cc4aa5f6e3f
Alex Dombroski <alexdombroski1@gmail.com>
2026-05-12 21:42:00 -0600

    feat: add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,59 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
+import { parser as tsParser, plugin as tsPlugin } from 'typescript-eslint';
+import astro from 'eslint-plugin-astro';
+
+export default defineConfig([
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/.astro/**'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      'no-console': ['warn', { allow: ['info', 'warn', 'error'] }],
+      eqeqeq: 'warn',
+      semi: 'warn',
+    },
+  },
+  {
+    files: ['packages/backend/**/*.{ts,mts,js,mjs,cjs}', '.husky/*.ts'],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  {
+    files: ['packages/frontend/**/*.{ts,mts,js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ['**/*.{ts,mts}'],
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2017,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      eqeqeq: 'warn',
+      semi: 'warn',
+      '@typescript-eslint/consistent-type-imports': 'warn',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn', // Use correct rule
+    },
+  },
+  ...astro.configs.recommended,
+]);
